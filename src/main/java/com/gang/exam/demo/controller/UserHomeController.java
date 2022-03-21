f1cd384ed4e501ec296437f37f2c34d84eea08da
GyeungHoon <ghp0415@naver.com>
2022-03-21 14:05:50 +0900

    게시물수정

--- src/main/java/com/gang/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/gang/exam/demo/controller/UserHomeController.java
@@ -1,25 +1,96 @@
 package com.gang.exam.demo.controller;
 
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.gang.exam.demo.vo.Article;
+
 @Controller
 public class UserHomeController {
-
-	@RequestMapping("/usr/home/main")
+	
+	@RequestMapping("/urs/home/getString")
 	@ResponseBody
-	public String showMain() {
-		return "안녕하세요.";
+	public String getString() {
+		return "HI";
 	}
-	@RequestMapping("/usr/home/main2")
+	
+	@RequestMapping("/urs/home/getInt")
 	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다";
+	public int getInt() {
+		return 10;
 	}
-	@RequestMapping("/usr/home/main3")
+	@RequestMapping("/urs/home/getFloat")
 	@ResponseBody
-	public String showMain3() {
-		return "또 만나요.";
+	public float getFloat() {
+		return 10.5f;
 	}
+	@RequestMapping("/urs/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 10.5;
+	}
+	@RequestMapping("/urs/home/getBoolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return true;
+	}
+	@RequestMapping("/urs/home/getCharacter")
+	@ResponseBody
+	public char getCharacter() {
+		return 'a';
+	}
+	
+	@RequestMapping("/urs/home/getMap")
+	@ResponseBody
+	public Map getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수나이", 22);
+		map.put("영희나이", 33);
+		return map;
+	}
+	
+	@RequestMapping("/urs/home/getList")
+	@ResponseBody
+	public List<String> getList() {
+		List<String> list = new ArrayList<>();
+		list.add("철수나이");
+		list.add("영희나이");
+		return list;
+	}
+	
+	@RequestMapping("/urs/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article= new Article();
+	
+		return article;
+	}
+	
+	@RequestMapping("/urs/home/getArticles")
+	@ResponseBody
+	public List<Article> getArticles() {
+		Article article1 = new Article(1, "제목1", "제목1");
+		Article article2 = new Article(2, "제목2", "제목1");
+		
+		List<Article> list = new ArrayList<>();
+		
+		list.add(article1);
+		list.add(article2);
+		
+		return list;
+	}
+	
+	
+	
+	
+	
+	
 }
