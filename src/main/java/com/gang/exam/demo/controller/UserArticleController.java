0d4e076eb674ad3d2b465d4034f31223b488134b
GyeungHoon <ghp0415@naver.com>
2022-03-21 14:43:51 +0900

    UsrArticleController가 가지고 있던 서비스 로직들을 ArticleService로 올김

--- src/main/java/com/gang/exam/demo/controller/UserArticleController.java
+++ src/main/java/com/gang/exam/demo/controller/UserArticleController.java
@@ -5,83 +5,25 @@ package com.gang.exam.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.gang.exam.demo.service.ArticleService;
 import com.gang.exam.demo.vo.Article;
 
 @Controller
 public class UserArticleController {
-	//인스턴스 변수 시작
+	@Autowired
 	private ArticleService articleService;
-	private int articlesLastId ;
-	private List<Article> articles ;
-	//인스턴스 변수 끝
-	/*
-	 * 
-	 */
-	//생성자
-	public UserArticleController() {
-		articlesLastId = 0; 
-		articles = new ArrayList<>();
-		makeTestData();
-	}
 	
-	// 서비스 메서드 시작
-	private void makeTestData() {
-		
-		for(int i=1; i<=10; i++) {
-	
-			String title = "제목" +i;
-			String body = "내용" +i;
-			writeArticle(title,body);
-		}
-		
-	}
-	public Article writeArticle(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		articlesLastId = id;
-		return article;
-		
-	}
-	
-	private Article getArticle(int id) {
-		
-		for(Article article : articles) {
-			if(article.getId() == id) {
-				return article;
-			}
-		}
-		
-		return null;
-	}
-	
-	private void deleteArticle(int id) {
-		
-		Article article = getArticle(id);
-		articles.remove(article);
-		
-	}
-	
-	private void modifyArticle(int id, String title, String body) {
-		
-		Article article = getArticle(id);
-		
-		article.setTitle(title);
-		article.setBody(body);
-	}
-
-	
-	// 서비스 메서드 끝
 	
 // 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-	Article article = writeArticle(title,body);
+	Article article = articleService.writeArticle(title,body);
 	
 		return article;
 		
@@ -91,8 +33,7 @@ public class UserArticleController {
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles(){
-		
-		return articles;
+		return articleService.getArticles();
 		
 	}
 	
@@ -100,13 +41,13 @@ public class UserArticleController {
 	@ResponseBody
 	public Object getArticleAction(int id){
 		
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
 			return id +"번 게시물은 존재하지 않습니다.";
 		}
 		
-		return articles;
+		return articleService.getArticle(id);
 		
 	}
 	
@@ -114,12 +55,12 @@ public class UserArticleController {
 	@ResponseBody
 	public String doDelete(int id){
 		
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
 			return id + "번 게시물이 존재하지 않습니다";
 		}
-		deleteArticle(id);
+		articleService.deleteArticle(id);
 		
 		return id + "번 게시물 삭제";
 		
@@ -129,12 +70,12 @@ public class UserArticleController {
 	@ResponseBody
 	public String doDelete(int id, String title, String body){
 		
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article == null) {
 			return id + "번 게시물이 존재하지 않습니다";
 		}
-		modifyArticle(id, title, body);
+		articleService.modifyArticle(id, title, body);
 		return id + "번 게시물 수정";
 		
 	}
--- src/main/java/com/gang/exam/demo/controller/UserHomeController.java
+++ src/main/java/com/gang/exam/demo/controller/UserHomeController.java
@@ -16,77 +16,13 @@ import com.gang.exam.demo.vo.Article;
 @Controller
 public class UserHomeController {
 	
-	@RequestMapping("/urs/home/getString")
+	@RequestMapping("/urs/home/main")
 	@ResponseBody
 	public String getString() {
-		return "HI";
+		return "안녕하세요";
 	}
 	
-	@RequestMapping("/urs/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 10;
-	}
-	@RequestMapping("/urs/home/getFloat")
-	@ResponseBody
-	public float getFloat() {
-		return 10.5f;
-	}
-	@RequestMapping("/urs/home/getDouble")
-	@ResponseBody
-	public double getDouble() {
-		return 10.5;
-	}
-	@RequestMapping("/urs/home/getBoolean")
-	@ResponseBody
-	public boolean getBoolean() {
-		return true;
-	}
-	@RequestMapping("/urs/home/getCharacter")
-	@ResponseBody
-	public char getCharacter() {
-		return 'a';
-	}
-	
-	@RequestMapping("/urs/home/getMap")
-	@ResponseBody
-	public Map getMap() {
-		Map<String, Object> map = new HashMap<>();
-		map.put("철수나이", 22);
-		map.put("영희나이", 33);
-		return map;
-	}
-	
-	@RequestMapping("/urs/home/getList")
-	@ResponseBody
-	public List<String> getList() {
-		List<String> list = new ArrayList<>();
-		list.add("철수나이");
-		list.add("영희나이");
-		return list;
-	}
-	
-	@RequestMapping("/urs/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-		Article article= new Article();
-	
-		return article;
-	}
-	
-	@RequestMapping("/urs/home/getArticles")
-	@ResponseBody
-	public List<Article> getArticles() {
-		Article article1 = new Article(1, "제목1", "제목1");
-		Article article2 = new Article(2, "제목2", "제목1");
-		
-		List<Article> list = new ArrayList<>();
-		
-		list.add(article1);
-		list.add(article2);
-		
-		return list;
-	}
+
 	
 	
 	
