258fa8eabd92fc25f7f3da34f5fdeb3f3982fb51
GyeungHoon <ghp0415@naver.com>
2022-03-24 14:56:10 +0900

    MemberRepository 추가 후, 회원가입 처리

---
+++ src/main/java/com/gang/exam/demo/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.gang.exam.demo.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface MemberRepository {
+
+	
+	void join(@Param("loginId")String loginId, @Param("loginPw")String loginPw,
+			@Param("name")String name, @Param("nickname")String nickname,
+			@Param("cellphoneNo")String cellphoneNo, @Param("email")String email);
+
+}
--- src/main/java/com/gang/exam/demo/service/MemberService.java
+++ src/main/java/com/gang/exam/demo/service/MemberService.java
@@ -1,16 +1,19 @@
 package com.gang.exam.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gang.exam.demo.repository.MemberRepository;
 
 @Service
 public class MemberService {
 
-	
-	//private MemberRepository memberRepository;
+	@Autowired
+	private MemberRepository memberRepository;
 
 	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
 		
-		//memberRepository.join( loginId,  loginPw,  name, nickname, cellphoneNo, email);
+		memberRepository.join( loginId,  loginPw,  name, nickname, cellphoneNo, email);
 	}
 
 }
