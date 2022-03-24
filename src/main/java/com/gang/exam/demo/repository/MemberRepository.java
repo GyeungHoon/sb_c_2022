c415a7a13f4701039311ad5b75569b2ad17a92f9
GyeungHoon <ghp0415@naver.com>
2022-03-24 15:27:59 +0900

    회원가입 완료 후, 생성된 회원 보여줌

--- src/main/java/com/gang/exam/demo/repository/MemberRepository.java
+++ src/main/java/com/gang/exam/demo/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.gang.exam.demo.repository;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.gang.exam.demo.vo.Member;
+
 @Mapper
 public interface MemberRepository {
 
@@ -10,5 +12,9 @@ public interface MemberRepository {
 	void join(@Param("loginId")String loginId, @Param("loginPw")String loginPw,
 			@Param("name")String name, @Param("nickname")String nickname,
 			@Param("cellphoneNo")String cellphoneNo, @Param("email")String email);
+
+	int getLastInsertId();
+
+	Member getMemberById(@Param("id")int id);
 
 }
