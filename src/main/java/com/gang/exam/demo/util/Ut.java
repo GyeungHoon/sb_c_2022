5613d1aac3d155ac7decdaf4b2bd43e39cd50e5b
GyeungHoon <ghp0415@naver.com>
2022-03-28 22:34:05 +0900

    게시물 삭제후 replace로 이동

--- src/main/java/com/gang/exam/demo/util/Ut.java
+++ src/main/java/com/gang/exam/demo/util/Ut.java
@@ -16,4 +16,24 @@ public class Ut {
 			
 		return String.format(format, args);
 	}
+
+	public static String jsHistoryBack(String msg) {
+	
+		if(msg == null) {
+			msg = "";
+		}
+		
+		return Ut.f("<script> const msg = '%s'.trim(); if(msg.lenght>0){alert(msg);} history.back() </script>", msg);
+	}
+
+	public static String jsReplace(String msg, String uri) {
+		
+		if(msg == null) {
+			msg = "";
+		}
+		if(uri == null) {
+			uri = "";
+		}
+		return Ut.f("<script> const msg = '%s'.trim(); if(msg.lenght>0){alert(msg);} location.replace('%s') </script>",msg, uri);
+	}
 }
