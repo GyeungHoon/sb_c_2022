32e8e1dfde0232bd4a55e8061b2dd876aeb2e9f3
GyeungHoon <ghp0415@naver.com>
2022-03-28 16:58:14 +0900

    게시물 상세페이지 작성자명 표시

--- src/main/java/com/gang/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/gang/exam/demo/repository/ArticleRepository.java
@@ -18,7 +18,7 @@ public interface ArticleRepository {
 	
 	public void writeArticle(@Param("memberId") int memberId, @Param("title")String title, @Param("body")String body);
 	
-	public Article getArticle(@Param("id") int id);
+	public Article getForPrintArticle(@Param("id") int id);
 	
 	
 	public void deleteArticle(@Param("id") int id);
