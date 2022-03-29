4f567244f79d31d0695e041227e45fc12c0beafe
GyeungHoon <ghp0415@naver.com>
2022-03-29 18:04:02 +0900

    NeedLogin 인터셉터 구현 및 등록

--- src/main/java/com/gang/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/gang/exam/demo/controller/UsrArticleController.java
@@ -27,7 +27,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public ResultData<Article> doAdd(HttpServletRequest req, String title, String body) {
-		Rq rq = new Rq(req);
+		Rq rq = (Rq) req.getAttribute("rq");
 
 		if (rq.isLogined() == false) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요.");
@@ -52,7 +52,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/list")
 	public String showList(HttpServletRequest req, Model model) {
 		
-		Rq rq = new Rq(req);
+		Rq rq = (Rq) req.getAttribute("rq");
 		
 		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId());
 
@@ -63,7 +63,7 @@ public class UsrArticleController {
 	
 	@RequestMapping("/usr/article/detail")
 	public String showDetail(HttpServletRequest req, Model model, int id) {
-		Rq rq = new Rq(req);
+		Rq rq = (Rq) req.getAttribute("rq");
 		
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
@@ -77,7 +77,7 @@ public class UsrArticleController {
 	@ResponseBody
 	public ResultData<Article> getArticle(HttpServletRequest req, int id) {
 		
-		Rq rq = new Rq(req);
+		Rq rq = (Rq) req.getAttribute("rq");
 		
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
@@ -91,7 +91,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(HttpServletRequest req, int id) {
-		Rq rq = new Rq(req);
+		Rq rq = (Rq) req.getAttribute("rq");
 
 		if (rq.isLogined() == false) {
 			return Ut.jsHistoryBack("로그인 후 이용해주세요.");
@@ -99,12 +99,14 @@ public class UsrArticleController {
 
 		Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
-		if (article.getMemberId() != rq.getLoginedMemberId()) {
-			return Ut.jsHistoryBack("권한이 없습니다.");
-		}
 
 		if (article == null) {
 			Ut.jsHistoryBack(Ut.f("%d번 게시물이 존재하지 않습니다.", id));
+		}
+
+		
+		if (article.getMemberId() != rq.getLoginedMemberId()) {
+			return Ut.jsHistoryBack("권한이 없습니다.");
 		}
 
 		articleService.deleteArticle(id);
@@ -115,7 +117,7 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
 	public ResultData<Article> doModify(HttpServletRequest req, int id, String title, String body) {
-		Rq rq = new Rq(req);
+		Rq rq = (Rq) req.getAttribute("rq");
 
 		if (rq.isLogined() == false) {
 			return ResultData.from("F-A", "로그인 후 이용해주세요.");
--- src/main/java/com/gang/exam/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/gang/exam/demo/interceptor/NeedLoginInterceptor.java
@@ -9,12 +9,11 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import com.gang.exam.demo.vo.Rq;
 
 @Component
-public class BeforeActionInterceptor implements HandlerInterceptor {
+public class NeedLoginInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		Rq rq = new Rq(req);
-		req.setAttribute("rq", rq);
+		System.out.println("로그인 필요");
 		
 		
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
