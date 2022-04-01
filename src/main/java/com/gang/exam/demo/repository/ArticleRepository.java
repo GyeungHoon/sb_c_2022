0f12eaad043fda07e8f8471657a8138335dd8058
GyeungHoon <ghp0415@naver.com>
2022-04-01 14:11:54 +0900

    게시물 검색어에따라 게시물 필터, 게시물 페이지메뉴에 검색어 유지

--- src/main/java/com/gang/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/gang/exam/demo/repository/ArticleRepository.java
@@ -27,7 +27,7 @@ public interface ArticleRepository {
 	public void modifyArticle(@Param("id")int id, @Param("title")String title, @Param("body")String body);
 
 	
-	public List<Article> getArticles(@Param("boardId")int boardId, int limitStart, int limitTake);
+	public List<Article> getArticles(@Param("boardId")int boardId, String searchKeywordTypeCode, String searchKeyword, int limitStart, int limitTake);
 
 	
 	public int getLastInsertId();
